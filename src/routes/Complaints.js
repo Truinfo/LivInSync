e8bfc7b93e315342264ccb8c370aa21bf7fe39cc
Truinfo <157565637+Truinfo@users.noreply.github.com>
2024-09-25 16:30:17 +0530

    Update Complaints.js

--- src/routes/Complaints.js
+++ src/routes/Complaints.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createComplaint, getAllComplaintsBySocietyId, getComplaintBySocietyId, updateComplaint, deleteComplaint } = require('../controllers/Complaints');
+const { createComplaint, getAllComplaintsBySocietyId, getComplaintBySocietyId, updateComplaint, deleteComplaint,updateComplaintStatus } = require('../controllers/Complaints');
 
 // Routes for complaints
 router.post('/createComplaint', createComplaint);
@@ -8,5 +8,6 @@ router.get('/getAllComplaints/:societyId', getAllComplaintsBySocietyId);
 router.get('/getComplaintBySocietyId/:societyId/:complaintId', getComplaintBySocietyId);
 router.put('/updateComplaint/:societyId/:complaintId', updateComplaint);
 router.delete('/deleteComplaint/:societyId/:complaintId', deleteComplaint);
+router.put('/updateComplaintStatus/:societyId/:complaintId', updateComplaintStatus);
 
 module.exports = router;
