619c042b1a718e176f29983750d5ae250d9db060
Truinfo <157565637+Truinfo@users.noreply.github.com>
2024-10-09 18:00:28 +0530

    Update Visitors.js

--- src/routes/Visitors.js
+++ src/routes/Visitors.js
@@ -37,5 +37,6 @@ router.get('/getFrequentVisitors/:societyId/:block/:flatNo', getFrequentVisitors
 router.get('/getPreApprovedVisitors/:societyId/:block/:flatNo', getPreApprovedVisitors);
 router.get('/getAllVisitorsbyFlatNo/:societyId/:block/:flatNo', getAllVisitorsbyFlatNo);
 router.delete('/deleteFrequentVisitor/:societyId/:block/:flatNo/:visitorId', deleteFrequentVisitors);
+router.delete('/deleteEntryVisitor/:societyId/:block/:flatNo/:visitorId', deleteEntryVisit);
 
 module.exports = router;
